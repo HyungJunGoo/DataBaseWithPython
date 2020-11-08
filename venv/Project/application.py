9fe1acc02f6203e7030998a3c67be3cf4eab0099
HyungJun Goo <9h0jun1115@gmail.com>
2020-11-09 01:53:09 +0900

    Need to Fix Search Button

--- venv/Project/application.py
+++ venv/Project/application.py
@@ -48,142 +48,258 @@ class DB_Queries:
         tuples = util.queryExecutor(db='kleague', sql=sql, params=params)
         return tuples
 
-    def selectPlayerUsingPosition (self, position):
-        sql = 'SELECT * FROM player WHERE position = %s'
-        params = (position)
+    def selectDistinctHeight(self):
+        sql = "SELECT DISTINCT height FROM player ORDER BY height ASC"
+        params = ()
         util = DB_Utils()
         tuples = util.queryExecutor(db='kleague', sql=sql, params=params)
         return tuples
+
+    def selectDistinctWeight(self):
+        sql = "SELECT DISTINCT weight FROM player ORDER BY weight ASC"
+        params = ()
+        util = DB_Utils()
+        tuples = util.queryExecutor(db='kleague', sql=sql, params=params)
+        return tuples
+
+    def selectPlayerUsingTeam (self, Team):
+        sql = 'SELECT * FROM player WHERE team_id = %s'
+        params = (Team)
+        util = DB_Utils()
+        tuples = util.queryExecutor(db='kleague', sql=sql, params=params)
+        return tuples
+
+    def selectPlayerUsingPosition(self, Position):
+        sql = 'SELECT * FROM player WHERE position = %s'
+        params = (Position)
+        util = DB_Utils()
+        tuples = util.queryExecutor(db='kleague', sql=sql, params=params)
+        return tuples
+
+    def intersection(self, team, position, nation):
+        sql = "SELECT * FROM player WHERE team_id = %s AND position = %s AND nation = %s"
+        sql = """PREPARE statement FROM 'SELECT * FROM player WHERE team_id = ? AND position = ? AND nation = ?;'
+                 SET @a = (%s, %s, %s);
+                 Execute statement using @a;
+                """
+        params = (team, position, nation)
+        util = DB_Utils()
+        tuples = util.queryExecutor(db='kleague', sql=sql, params=params)
+        return tuples
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setupUI()
+        self.condition_team_id = self.comboBox1.currentText()
+        self.condition_position = self.comboBox2.currentText()
+        self.condition_nation = self.comboBox3.currentText()
+        self.condition_height = self.comboBox4.currentText()
+        self.condition_weight = self.comboBox5.currentText()
 
     def setupUI(self):
         self.setWindowTitle("Application")
         self.setGeometry(0, 0, 600, 900)
 
         # set widget
-        title = QLabel("선수검색", self)
-        title.move(100,0)
-        comboBox1 = QComboBox(self)
-        comboTitle1 = QLabel("팀 명 : ", self)
-        comboBox2 = QComboBox(self)
-        comboTitle2 = QLabel("포지션", self)
-        comboBox3 = QComboBox(self)
-        comboTitle3 = QLabel("출신국", self)
-        comboBox4 = QComboBox(self)
-        comboTitle4 = QLabel("키", self)
-        radioButton1 = QRadioButton("이상", self)
-        radioButton2 = QRadioButton("이하", self)
-        comboBox5 = QComboBox(self)
-        comboTitle5 = QLabel("몸무게", self)
-        radioButton3 = QRadioButton("이상", self)
-        radioButton4 = QRadioButton("이하", self)
+        self.title = QLabel("선수검색", self)
+        self.title.move(100,0)
+        self.comboBox1 = QComboBox(self)
+        self.comboBox1.activated.connect(self.team_id_activated)
+        self.comboTitle1 = QLabel("팀 명 : ", self)
+        self.comboBox2 = QComboBox(self)
+        self.comboBox2.activated.connect(self.position_activated)
+        self.comboTitle2 = QLabel("포지션", self)
+        self.comboBox3 = QComboBox(self)
+        self.comboBox3.activated.connect(self.nation_activated)
+        self.comboTitle3 = QLabel("출신국", self)
+        self.comboBox4 = QComboBox(self)
+        self.comboTitle4 = QLabel("키", self)
+        self.radioButton1 = QRadioButton("이상", self)
+        self.radioButton2 = QRadioButton("이하", self)
+        self.comboBox5 = QComboBox(self)
+        self.comboTitle5 = QLabel("몸무게", self)
+        self.radioButton3 = QRadioButton("이상", self)
+        self.radioButton4 = QRadioButton("이하", self)
 
-        pushButton1 = QPushButton("초기화", self)
-        pushButton2 = QPushButton("검색", self)
+        self.pushButton1 = QPushButton("초기화", self)
+        # pushButton1.clicked.connect()
+        self.pushButton2 = QPushButton("검색", self)
+        self.pushButton2.clicked.connect(self.searchButton_Clicked)
 
-        radioButton5 = QRadioButton("CSV", self)
-        radioButton6 = QRadioButton("JSON", self)
-        radioButton7 = QRadioButton("XML", self)
-        pushButton3 = QPushButton("저장", self)
+
+        self.radioButton5 = QRadioButton("CSV", self)
+        self.radioButton6 = QRadioButton("JSON", self)
+        self.radioButton7 = QRadioButton("XML", self)
+        self.pushButton3 = QPushButton("저장", self)
 
 
 
         # set layout
-        qhinnerLayout1 = QHBoxLayout()
-        qhinnerLayout1.addWidget(comboTitle1)
-        qhinnerLayout1.addWidget(comboBox1)
-        qhinnerLayout1.addWidget(comboTitle2)
-        qhinnerLayout1.addWidget(comboBox2)
-        qhinnerLayout1.addWidget(comboTitle3)
-        qhinnerLayout1.addWidget(comboBox3)
-        qhinnerLayout1.addWidget(pushButton1)
+        self.qhinnerLayout1 = QHBoxLayout()
+        self.qhinnerLayout1.addWidget(self.comboTitle1)
+        self.qhinnerLayout1.addWidget(self.comboBox1)
+        self.qhinnerLayout1.addWidget(self.comboTitle2)
+        self.qhinnerLayout1.addWidget(self.comboBox2)
+        self.qhinnerLayout1.addWidget(self.comboTitle3)
+        self.qhinnerLayout1.addWidget(self.comboBox3)
+        self.qhinnerLayout1.addWidget(self.pushButton1)
 
-        qhinnerLayout2 = QHBoxLayout()
+        self.qhinnerLayout2 = QHBoxLayout()
         # for height
-        qHheightLayout = QHBoxLayout()
-        qHheightLayout.addWidget(comboTitle4)
-        qHheightLayout.addWidget(comboBox4)
-        heightRadioLayout = QHBoxLayout()
-        heightRadioLayout.addWidget(radioButton1)
-        heightRadioLayout.addWidget(radioButton2)
-        heightRadioGroup = QGroupBox()
-        heightRadioGroup.setFlat(True)
-        heightRadioGroup.setLayout(heightRadioLayout)
-        qHheightLayout.addWidget(heightRadioGroup)
+        self.qHheightLayout = QHBoxLayout()
+        self.qHheightLayout.addWidget(self.comboTitle4)
+        self.qHheightLayout.addWidget(self.comboBox4)
+        self.heightRadioLayout = QHBoxLayout()
+        self.heightRadioLayout.addWidget(self.radioButton1)
+        self.heightRadioLayout.addWidget(self.radioButton2)
+        self.heightRadioGroup = QGroupBox()
+        self.heightRadioGroup.setFlat(True)
+        self.heightRadioGroup.setLayout(self.heightRadioLayout)
+        self.qHheightLayout.addWidget(self.heightRadioGroup)
         # for weight
-        qHweightLayout = QHBoxLayout()
-        qHweightLayout.addWidget(comboTitle5)
-        qHweightLayout.addWidget(comboBox5)
-        weightRadioLayout = QHBoxLayout()
-        weightRadioLayout.addWidget(radioButton3)
-        weightRadioLayout.addWidget(radioButton4)
-        weightRadioGroup = QGroupBox()
-        weightRadioGroup.setFlat(True)
-        weightRadioGroup.setLayout(weightRadioLayout)
-        qHweightLayout.addWidget(weightRadioGroup)
+        self.qHweightLayout = QHBoxLayout()
+        self.qHweightLayout.addWidget(self.comboTitle5)
+        self.qHweightLayout.addWidget(self.comboBox5)
+        self.weightRadioLayout = QHBoxLayout()
+        self.weightRadioLayout.addWidget(self.radioButton3)
+        self.weightRadioLayout.addWidget(self.radioButton4)
+        self.weightRadioGroup = QGroupBox()
+        self.weightRadioGroup.setFlat(True)
+        self.weightRadioGroup.setLayout(self.weightRadioLayout)
+        self.qHweightLayout.addWidget(self.weightRadioGroup)
 
-        qhinnerLayout2.addLayout(qHheightLayout)
-        qhinnerLayout2.addLayout(qHweightLayout)
-        qhinnerLayout2.addWidget(pushButton2)
+        self.qhinnerLayout2.addLayout(self.qHheightLayout)
+        self.qhinnerLayout2.addLayout(self.qHweightLayout)
+        self.qhinnerLayout2.addWidget(self.pushButton2)
 
-        qhinnerLayout3 = QHBoxLayout()
-        qhinnerLayout3.addWidget(radioButton5)
-        qhinnerLayout3.addWidget(radioButton6)
-        qhinnerLayout3.addWidget(radioButton7)
-        qhinnerLayout3.addWidget(pushButton3)
+        self.qhinnerLayout3 = QHBoxLayout()
+        self.qhinnerLayout3.addWidget(self.radioButton5)
+        self.qhinnerLayout3.addWidget(self.radioButton6)
+        self.qhinnerLayout3.addWidget(self.radioButton7)
+        self.qhinnerLayout3.addWidget(self.pushButton3)
 
 
 
         query = DB_Queries()
         players = query.selectAllplayer()
-        print(players[0])
+        # print(players[0])
         # Table settings
         columnNames = list(players[0].keys())
-        tableWidget = QTableWidget(len(players), 13)
-        tableWidget.setHorizontalHeaderLabels(columnNames)
+        self.tableWidget = QTableWidget(len(players), 13)
+        self.tableWidget.setHorizontalHeaderLabels(columnNames)
 
-        # Temp
+        # # Temp
         i = 0
 
         for player in players:
-            i += 1
             j = 0
             for columnName in columnNames:
-                tableWidget.setItem(i, j, QTableWidgetItem(str(player[columnName])))
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(player[columnName])))
                 # print(player[columnName])
                 j+=1
+            i += 1
 
 
-        # Team settings (comboBox 3)
+        # Team settings (comboBox 1)
         teams = query.selectDistinctTeam()
         teamName = list(teams[0].keys())[0]
         teamItems = ['없음' if team[teamName] == None else team[teamName] for team in teams]
-        comboBox1.addItems(teamItems)
+        teamItems.append('없음')
+        self.comboBox1.addItems(teamItems)
+        self.comboBox1.setCurrentText('없음')
 
         # Position settings (comboBox2)
         positions = query.selectDistinctPosition()
         positionName = list(positions[0].keys())[0]
         positionItems = ['없음' if position[positionName] == None else position[positionName] for position in positions]
-        comboBox2.addItems(positionItems)
+        self.comboBox2.addItems(positionItems)
+        self.comboBox2.setCurrentText('없음')
 
         # Nation settings (comboBox 3)
         nations = query.selectDistinctNation()
         nationName = list(nations[0].keys())[0]
         nationItems = ['대한민국' if nation[nationName] == None else nation[nationName] for nation in nations]
-        comboBox3.addItems(nationItems)
-        # Out Layer
-        qvinnerLayout = QVBoxLayout()
-        qvinnerLayout.addWidget(title)
-        qvinnerLayout.addLayout(qhinnerLayout1)
-        qvinnerLayout.addLayout(qhinnerLayout2)
-        qvinnerLayout.addWidget(tableWidget)
-        qvinnerLayout.addLayout(qhinnerLayout3)
+        nationItems.append('모두')
+        self.comboBox3.addItems(nationItems)
+        self.comboBox3.setCurrentText("모두")
 
-        self.setLayout(qvinnerLayout)
+        # Height settings (comboBox 4)
+        heights = query.selectDistinctHeight()
+        wholeHeights = list(heights[0].keys())[0]
+        heightItems = ['없음' if height[wholeHeights] == None else str(height[wholeHeights]) for height in heights]
+        self.comboBox4.addItems(heightItems)
+
+        # Weight settings (comboBox 5)
+        weights = query.selectDistinctWeight()
+        wholeWeights = list(weights[0].keys())[0]
+        weightItems = ['없음' if weight[wholeWeights] == None else str(weight[wholeWeights]) for weight in weights]
+        self.comboBox5.addItems(weightItems)
+        # Out Layer
+
+        self.qvinnerLayout = QVBoxLayout()
+        self.qvinnerLayout.addWidget(self.title)
+        self.qvinnerLayout.addLayout(self.qhinnerLayout1)
+        self.qvinnerLayout.addLayout(self.qhinnerLayout2)
+        self.qvinnerLayout.addWidget(self.tableWidget)
+        self.qvinnerLayout.addLayout(self.qhinnerLayout3)
+
+        self.setLayout(self.qvinnerLayout)
+
+    def team_id_activated(self):
+        self.condition_team_id = self.comboBox1.currentText()
+        print(self.condition_team_id)
+        pass
+
+    def position_activated(self):
+        self.condition_position = self.comboBox2.currentText()
+        print(self.condition_position)
+        pass
+
+    def nation_activated(self):
+        self.condition_nation = self.comboBox3.currentText()
+        print(self.condition_nation)
+        pass
+
+    def searchButton_Clicked(self):
+
+        i = 0
+        query = DB_Queries()
+        if self.condition_team_id == '없음' and self.condition_position == '없음' and self.condition_nation == '모두':
+            players = query.selectAllplayer()
+        else:
+            if self.condition_team_id == '없음':
+                self.condition_team_id = '*'
+            if self.condition_position == '없음':
+                self.condition_position = '*'
+            if self.condition_nation == '모두':
+                self.condition_nation = '*'
+            players = query.intersection(self.condition_team_id, self.condition_position, self.condition_nation)
+
+        # if self.condition_team_id == '없음':
+        #     players_by_team_id = query.selectAllplayer()
+        # else:
+        #     players_by_team_id = query.selectPlayerUsingTeam(self.condition_team_id)
+        #
+        # if self.condition_position == '없음':
+        #     players_by_position = query.selectAllplayer()
+        # else:
+        #     players_by_position = query.selectPlayerUsingPosition(self.condition_position)
+        #
+        # players = list(set(players_by_team_id)&set(players_by_position))
+        print(players)
+        self.tableWidget.setRowCount(len(players))
+        columnNames = list(players[0].keys())
+
+        for player in players:
+            print(player)
+            j = 0
+            for columnName in columnNames:
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(player[columnName])))
+                j += 1
+            i += 1
+
 
 
 
